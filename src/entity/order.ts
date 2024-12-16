2de3570cdadc57e72a0ebdb4fbfe92fb1171ca59
diegodevtech <diegodevtechnology@gmail.com>
2024-12-16 17:02:15 -0400

    feat: adds product entity and its unit tests

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,13 +1,33 @@
 import OrderItem from "./order_item";
 
 export default class Order {
-  _id: string;
-  _customerId: string;
-  _items: OrderItem[] = [];
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[] = [];
+  private _total: number;
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
+    this._total = this.total();
+
+    this.validate();
+  }
+
+  validate() {
+    if(this._id.length === 0) {
+      throw new Error("ID is required.")
+    }
+    if (this._customerId.length === 0) {
+      throw new Error("Customer ID is required.");
+    }
+    if (this._items.length === 0) {
+      throw new Error("A list of items is required and must have length greater than zero.");
+    }    
+  }
+
+  total(): number {
+    return this._items.reduce((aac, item) => aac + item._price, 0);
   }
 }
