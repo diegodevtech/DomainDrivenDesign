fd907f4654f9b52128c1fba44897fd9520aa7924
diegodevtech <diegodevtechnology@gmail.com>
2024-12-18 12:16:30 -0400

    feat: adjusts customer entity to have rewardPoints attr

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,10 +1,11 @@
 import Address from "./address";
 
 export default class Customer {
-    _id: string;
-    _name: string;
-    _address!: Address;
-    _active: boolean = false;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -16,8 +17,16 @@ export default class Customer {
         return this._active;
     }
 
+    get id(): string {
+        return this._id
+    }
+
     get name(): string {
         return this._name
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
     }
 
     validate() {
@@ -43,6 +52,10 @@ export default class Customer {
     
     deactivate() {
         this._active = false
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
     }
 
     setAddress(address: Address) {
