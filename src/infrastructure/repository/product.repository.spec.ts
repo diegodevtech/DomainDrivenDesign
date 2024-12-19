a9ca88d7c051765d9091de1c555b5cbfcbed086b
diegodevtech <diegodevtechnology@gmail.com>
2024-12-18 20:34:05 -0400

    feat: adds create product repository test

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
 
 describe("Product Repository Test", () => {
   let sequelize: Sequelize;
@@ -17,6 +18,21 @@ describe("Product Repository Test", () => {
   });
   afterEach(async () => {
       await sequelize.close();
+  })
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 100)
+
+    await productRepository.create(product)
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } })
+
+    expect(productModel.toJSON).toStrictEqual({
+      id: "123",
+      name: "Product 1",
+      price: 100
+    })
   })
 
 });
