aee0776647bf881167f6a00102db9ab0fa45d254
diegodevtech <diegodevtechnology@gmail.com>
2025-01-10 21:25:03 -0400

    feat: adds event dispatcher and first product events and handlers

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,13 @@
+import EventDispatcher from "./event-dispatcher";
+
+describe("Event Domain Tests", () => {
+  it("should register an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
+  })
+})
